7c91269795207c279e2ff9ec8514e3593e3b7e75
gdebasish611-bit <YOUR_GITHUB_EMAIL>
2026-09-19 22:13:31 +0530

    Fix static files for Render

--- hello/settings.py
+++ hello/settings.py
@@ -47,9 +47,7 @@ if render_hostname:
 # =========================================================
 
 INSTALLED_APPS = [
-
     "home.apps.HomeConfig",
-
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -64,19 +62,12 @@ INSTALLED_APPS = [
 # =========================================================
 
 MIDDLEWARE = [
-
     "django.middleware.security.SecurityMiddleware",
-
     "django.contrib.sessions.middleware.SessionMiddleware",
-
     "django.middleware.common.CommonMiddleware",
-
     "django.middleware.csrf.CsrfViewMiddleware",
-
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-
     "django.contrib.messages.middleware.MessageMiddleware",
-
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
@@ -93,26 +84,17 @@ ROOT_URLCONF = "hello.urls"
 # =========================================================
 
 TEMPLATES = [
-
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-
         "DIRS": [
             BASE_DIR / "templates"
         ],
-
         "APP_DIRS": True,
-
         "OPTIONS": {
-
             "context_processors": [
-
                 "django.template.context_processors.request",
-
                 "django.contrib.auth.context_processors.auth",
-
                 "django.contrib.messages.context_processors.messages",
-
             ],
         },
     },
@@ -131,11 +113,8 @@ WSGI_APPLICATION = "hello.wsgi.application"
 # =========================================================
 
 DATABASES = {
-
     "default": {
-
         "ENGINE": "django.db.backends.sqlite3",
-
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
@@ -146,22 +125,18 @@ DATABASES = {
 # =========================================================
 
 AUTH_PASSWORD_VALIDATORS = [
-
     {
         "NAME":
         "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-
     {
         "NAME":
         "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
-
     {
         "NAME":
         "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
-
     {
         "NAME":
         "django.contrib.auth.password_validation.NumericPasswordValidator",
@@ -187,6 +162,9 @@ USE_TZ = True
 # =========================================================
 
 STATIC_URL = "static/"
+
+# Render ke liye STATIC_ROOT
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
